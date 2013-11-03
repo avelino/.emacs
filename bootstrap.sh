e0924d79ef9443dcbbba9106b787608434ed61ad
Thiago Avelino <thiagoavelinoster@gmail.com>
2013-11-03 11:25:48 -0200

    load git submodule

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,4 +1,6 @@
 for i in ~/.emacs ~/.emacs.d; do [ -e $i ] && mv $i $i.old; done
-cd ~/
 git clone https://github.com/avelino/.emacs.git ~/.emacs.d
+cd ~/.emacs.d/
+git submodule init
+git submodule update
 ln -s ~/.emacs.d/.emacs ~/.emacs
