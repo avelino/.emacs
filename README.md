ec32db00602bae9572983bfa605bd70f5f70badc
Thiago Avelino <thiagoavelinoster@gmail.com>
2013-11-03 10:22:42 -0200

    Create @avelino/.emacs install via bootstrap

--- README.md
+++ README.md
@@ -1,3 +1,7 @@
 # .emacs
 
 Love yo faces!
+
+## Installation
+
+    curl https://raw.github.com/avelino/.emacs/master/bootstrap.sh -o - | sh 
---
+++ bootstrap.sh
@@ -0,0 +1,4 @@
+for i in ~/.emacs ~/.emacs.d; do [ -e $i ] && mv $i $i.old; done
+cd ~/
+git clone https://github.com/avelino/.emacs.git ~/.emacs.d
+ln -s ~/.emacs.d/emacs ~/.emacs
