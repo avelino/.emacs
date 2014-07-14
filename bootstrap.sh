6df5a99fd87656aa3b610c5ca6469939ce916f19
Thiago Avelino <thiagoavelinoster@gmail.com>
2014-07-14 20:28:24 -0300

    remove bootstrap script

--- bootstrap.sh
+++
@@ -1,6 +0,0 @@
-for i in ~/.emacs ~/.emacs.d; do [ -e $i ] && mv $i $i.old; done
-git clone https://github.com/avelino/.emacs.git ~/.emacs.d
-cd ~/.emacs.d/
-git submodule init
-git submodule update
-ln -s ~/.emacs.d/.emacs ~/.emacs
