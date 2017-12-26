cee7b3d392ada38a5f2cd19becec73bfcfe576a2
Avelino <t@avelino.xxx>
2017-12-26 01:49:14 -0200

    upgrade rum-mode to v 1.2

--- provide/rum-mode.el
+++ provide/rum-mode.el
@@ -1,24 +1,71 @@
-;;; rum-mode.el --- Major mode for Rum code
+;;; rum-mode.el --- Major mode for Rum programming language
 
-;; Copyright (C) 2017 Rum Language
-;;
+;; Copyright 2017 The Rum Language Authors.  All rights reserve
+;; Use of this source code is governed by a MIT
+;; license that can be found in the LICENSE file.
+
 ;; Authors: Avelino <t@avelino.xxx>
 ;; URL: https://github.com/rumlang/rum-mode
-;; Version: 1.0
+;; Version: 1.2
 ;; Keywords: rum, languages, lisp
-
+;; Package-Requires: ((emacs "24"))
+;;
 ;; This file is not part of GNU Emacs.
 
+;;; Commentary:
+
+;; rum-mode is a major mode for editing code written in Rum
+;; Language.
 
 ;;; Code:
 
-(require 'cl)
+(eval-when-compile
+  (require 'cl-lib))
 
+;; all rum keywords
+(defconst rum-keywords
+  (regexp-opt
+   '("package"
+     "breack"
+     "continue"
+     "for"
+     "if"
+     "else"
+     "import"
+     "let"
+     "return"
+     "struct"
+     "case"
+     "default"
+     "print"
+     "sprint"
+     "lambda") 'words))
+
+;; all rum functions name
+(defconst rum-functions
+  (regexp-opt
+   '("def") 'words))
+
+;; all rum types name
+(defconst rum-types
+  (regexp-opt
+   '("struct"
+     "map"
+     "array") 'words))
+
+;; create the list for font-lock.
+;; each category of keyword is given a particular face
 (defconst rum-font-lock-keywords
   (list
-   '("\\<\\(package\\|break\\|continue\\|for\\|if\\|else\\|import\\|let\\|map\\|return\\|struct\\|def\\|case\\|default\\|print\\|sprintf\\|def\\|lambda\\|array\\)\\>" . font-lock-builtin-face)
-   '("\\<\\(struct\\)\\s-+\\(\\w+\\)\\>" (1 font-lock-keyword-face) (2 font-lock-type-face))
-   '("\\<\\(def\\)\\s-+\\(\\w+\\)\\>" (1 font-lock-keyword-face) (2 font-lock-function-name-face))))
+   `(,rum-keywords . font-lock-builtin-face)
+   `(,rum-functions . font-lock-function-name-face)
+   `(,rum-types . font-lock-type-face)
+   `(,"\\<\\(struct\\)\\s-+\\(\\w+\\)\\>"
+     (1 font-lock-keyword-face)
+     (2 font-lock-type-face))
+   `(,"\\<\\(def\\)\\s-+\\(\\w+\\)\\>"
+     (1 font-lock-keyword-face)
+     (2 font-lock-function-name-face))))
 
 (define-derived-mode rum-mode lisp-mode "rum"
   "Major mode for editing rum script"
