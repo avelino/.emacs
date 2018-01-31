bdb0a7348907c46c415e3c6406097ec33dbcd6c7
Avelino <t@avelino.xxx>
2018-01-31 11:29:14 -0200

    added jvm modes

---
+++ provide/jvm-lang.el
@@ -0,0 +1,15 @@
+; jvm languages
+
+(use-package clojure-mode)
+
+(use-package ensime
+  :ensure t
+  :pin melpa)
+
+(use-package sbt-mode
+  :pin melpa)
+
+(use-package scala-mode
+  :pin melpa)
+
+(provide 'jvm)
