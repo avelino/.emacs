dbe26333e040245577220b9d830ff812faac3c2a
Avelino <t@avelino.xxx>
2018-02-05 11:45:41 -0200

    remove persp functions

--- provide/functions.el
+++ provide/functions.el
@@ -109,43 +109,6 @@ If point was already at that position, move point to beginning of line."
   (interactive)
   (mapc 'kill-buffer (buffer-list)))
 
-;; (defun persp/get-root (branch-name)
-;;   (let ((current-project (projectile-project-name)))
-;;     (message current-project)
-;;     (message branch-name)
-;;     (if (and (not current-project) (not branch-name))
-;;       (error "Could not find persp root"))
-
-;;     (if (and current-project branch-name)
-;; 	(concat current-project "-" branch-name)
-;;       (if current-project
-;; 	  current-project
-;; 	branch-name))))
-
-
-;; (defun persp/close-perspective (&optional project-root closed-branch)
-;;   (interactive)
-;;   (let* ((current-branch (if closed-branch
-;; 			    closed-branch
-;; 			  (magit-get-current-branch)))
-;; 	 (persp-project-root (if project-root
-;; 				 project-root
-;; 			       (persp/get-root current-branch))))
-;;     (if persp-project-root
-;; 	(progn
-;; 	  (message (concat "Saving " persp-project-root ".persp"))
-;; 	  (persp-save-state-to-file (concat persp-project-root ".persp"))
-;; 	  (close-all-buffers)))))
-
-;; (defun persp/switch-to-current-branch-persp ()
-;;   (interactive)
-;;   (let ((closed-branch (magit-get-previous-branch))
-;; 	(persp-project-root (persp/get-root (magit-get-current-branch)))
-;;     (persp/close-perspective persp-project-root closed-branch))
-;;     (message "Closed perspective")
-;;     (message (concat "Loading " persp-project-root ".persp"))
-;;     (persp-load-state-from-file (concat persp-project-root ".persp"))))
-
 (defun codenav-get-candidates ()
   "Fetch list of sorted imenu candidates."
   (let* ((items (counsel-imenu-get-candidates-from (imenu--make-index-alist)))
