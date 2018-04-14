21a3161c74a70d7cc3f22f59c88e0b9999011f90
Avelino <t@avelino.xxx>
2018-04-14 09:17:54 -0400

    added auto-package-update upgrade all package (by melpa) on open emacs/server

--- provide/extensions.el
+++ provide/extensions.el
@@ -189,7 +189,12 @@
   :init
   (add-hook 'after-init-hook #'server-start t))
 
-(use-package yasnippet-snippets)
+(use-package auto-package-update
+  :ensure t
+  :config
+  (setq auto-package-update-delete-old-versions t
+	auto-package-update-interval 4)
+     (auto-package-update-maybe))
 
 (provide 'extensions)
 
