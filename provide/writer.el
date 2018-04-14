a36e0524b8b671abc019b6caa8da84f73ec3a3cb
Avelino <t@avelino.xxx>
2018-04-14 09:24:15 -0400

    used taskpaper by melpa repo

--- provide/writer.el
+++ provide/writer.el
@@ -26,16 +26,16 @@
   (setq deft-extensions '("md" "txt" "tex" "org"))
   (setq deft-directory "~/Dropbox/Notes"))
 
-;; (use-package taskpaper-mode) ;; not yet Melpa repo - https://github.com/melpa/melpa/pull/5349
-(require 'taskpaper-mode)
-(add-to-list 'auto-mode-alist '("\\.papertask\\'" . taskpaper-mode))
-(add-to-list 'auto-mode-alist '("\\.todo\\'" . taskpaper-mode))
-(setq taskpaper-tag-faces
-      '(("done"    . (:foreground "green" :weight bold :underline t))
-	("waiting" . (:foreground "purple" :weight bold :underline t))
-        ("today"   . (:foreground "blue" :weight bold :underline t))
-	("flagged" . (:foreground "red" :weight bold :underline t))
-        ("due"     . (:foreground "orange" :weight bold :underline t))))
+(use-package 'taskpaper-mode
+  :config
+  (add-to-list 'auto-mode-alist '("\\.papertask\\'" . taskpaper-mode))
+  (add-to-list 'auto-mode-alist '("\\.todo\\'" . taskpaper-mode))
+  (setq taskpaper-tag-faces
+	'(("done"    . (:foreground "green" :weight bold :underline t))
+	  ("waiting" . (:foreground "purple" :weight bold :underline t))
+	  ("today"   . (:foreground "blue" :weight bold :underline t))
+	  ("flagged" . (:foreground "red" :weight bold :underline t))
+	  ("due"     . (:foreground "orange" :weight bold :underline t)))))
 
 (use-package apib-mode
   :config
