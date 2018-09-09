99b4baf2ca3c0c03dac6944800e2529d7a4a111e
Avelino <t@avelino.xxx>
2018-09-09 06:21:33 -0300

    web mode: active auto closing and engine detection

--- provide/web-lang.el
+++ provide/web-lang.el
@@ -7,8 +7,10 @@
 (use-package web-mode
   :config
   (setq web-mode-markup-indent-offset 2
-        web-mode-css-indent-offset 2
-        web-mode-code-indent-offset 2
+	web-mode-css-indent-offset 2
+	web-mode-code-indent-offset 2
+	web-mode-enable-auto-closing t
+	web-mode-enable-engine-detection t
 	web-mode-enable-current-element-highlight t)
 
   (add-to-list 'auto-mode-alist '("\\.js\\'" . web-mode))
