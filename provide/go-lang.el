d49718faab1ee59024f523a76d6fa4f140023023
Avelino <t@avelino.xxx>
2018-12-07 11:57:15 -0200

    golang: load all package on go-mode init

--- provide/go-lang.el
+++ provide/go-lang.el
@@ -13,10 +13,6 @@
 ;; Snag the user's PATH and GOPATH
 (setenv "GOPATH" "/Users/avelino")
 
-(use-package go-guru
-  :init
-  (go-guru-hl-identifier-mode))
-
 (use-package go-mode
   :config
   ; Use goimports instead of go-fmt
@@ -32,50 +28,56 @@
 			     (local-set-key (kbd "C-c C-r") 'go-remove-unused-imports)
   			     (local-set-key (kbd "C-c C-g") 'go-goto-imports)
 			     (set (make-local-variable 'company-backends) '(company-go))
-			     (company-mode))))
+			     (company-mode)))
 
-(use-package go-errcheck)
+  :init
+  (use-package go-guru
+    :init
+    (go-guru-hl-identifier-mode))
 
-(use-package go-add-tags)
 
-(use-package company-go
-  :after go-mode
-  :bind (:map go-mode-map
-  ; Godef jump key binding
-  ("M-." . godef-jump)))
+  (use-package go-errcheck)
 
-(use-package flymake-go)
+  (use-package go-add-tags)
 
-(use-package flycheck-gometalinter
-  :ensure t
-  :config
-  (progn
-    (flycheck-gometalinter-setup))
-  (setq
-   ;; skips 'vendor' directories and sets GO15VENDOREXPERIMENT=1
-   flycheck-gometalinter-vendor         t
-   ;; only show errors
-   flycheck-gometalinter-errors-only    t
-   ;; only run fast linters
-   flycheck-gometalinter-fast           t
-   ;; use in tests files
-   flycheck-gometalinter-test           t
-   ;; Only enable selected linters
-   flycheck-gometalinter-enable-linters '("golint" "gotype" "gocyclo")
-   ;; Set different deadline (default: 5s)
-   flycheck-gometalinter-deadline       "10s"))
+  (use-package company-go
+    :after go-mode
+    :bind (:map go-mode-map
+					; Godef jump key binding
+		("M-." . godef-jump)))
 
-(use-package go-eldoc
-  :config
-  (add-hook 'go-mode-hook 'go-eldoc-setup))
+  (use-package flymake-go)
+
+  (use-package flycheck-gometalinter
+    :ensure t
+    :config
+    (progn
+      (flycheck-gometalinter-setup))
+    (setq
+     ;; skips 'vendor' directories and sets GO15VENDOREXPERIMENT=1
+     flycheck-gometalinter-vendor         t
+     ;; only show errors
+     flycheck-gometalinter-errors-only    t
+     ;; only run fast linters
+     flycheck-gometalinter-fast           t
+     ;; use in tests files
+     flycheck-gometalinter-test           t
+     ;; Only enable selected linters
+     flycheck-gometalinter-enable-linters '("golint" "gotype" "gocyclo")
+     ;; Set different deadline (default: 5s)
+     flycheck-gometalinter-deadline       "10s"))
+
+  (use-package go-eldoc
+    :config
+    (add-hook 'go-mode-hook 'go-eldoc-setup))
+
+  (use-package go-autocomplete))
 
 (defun setup-go-mode-compile ()
   "Customize compile command to run go build"
   (if (not (string-match "go" compile-command))
       (set (make-local-variable 'compile-command)
            "go build -v && go test -v && go vet")))
-
-(use-package go-autocomplete)
 
 (provide 'go-lang)
 
