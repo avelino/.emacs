3479f9b4734010ad2e977341e8002b057a1ca98a
Avelino <t@avelino.xxx>
2019-10-18 10:45:30 -0300

    python: install pyenv and black run after save

--- provide/python-lang.el
+++ provide/python-lang.el
@@ -11,10 +11,12 @@
 ;;
 ;;; Code:
 
+
 (use-package elpy
-    :init
-    (add-to-list 'auto-mode-alist '("\\.py$" . python-mode))
-    :bind (:map elpy-mode-map
+  :ensure t
+  :init
+  (add-to-list 'auto-mode-alist '("\\.py$" . python-mode))
+  :bind (:map elpy-mode-map
 	      ("<M-left>" . nil)
 	      ("<M-right>" . nil)
 	      ("<M-S-left>" . elpy-nav-indent-shift-left)
@@ -22,22 +24,39 @@
 	      ("M-." . elpy-goto-definition)
 	      ("M-," . pop-tag-mark)
 	      ("C-c C-s" . nil))
-    :config
-    (add-hook 'python-mode-hook 'py-autopep8-enable-on-save)
-    flycheck-python-flake8-executable "/usr/local/bin/flake8"
-    (setq elpy-rpc-backend "jedi"))
+  :config
+  (add-hook 'python-mode-hook 'py-autopep8-enable-on-save)
+  flycheck-python-flake8-executable "/usr/local/bin/flake8"
+  (setenv "IPY_TEST_SIMPLE_PROMPT" "1")
+  (setq
+   elpy-rpc-backend "jedi"
+   python-shell-interpreter "ipython3"
+   python-shell-interpreter-args "-i"))
+
+(use-package pyvenv
+  :ensure t)
+
+(use-package blacken
+  :ensure t)
 
 (use-package python
+  :ensure t
   :mode ("\\.py" . python-mode)
   :config
+  (add-hook 'python-mode-hook 'blacken-mode)
   (setq python-indent-offset 4)
   (elpy-enable))
 
 (use-package pip-requirements
+  :ensure t
   :config
   (add-hook 'pip-requirements-mode-hook #'pip-requirements-auto-complete-setup))
 
-(use-package py-autopep8)
+(use-package py-autopep8
+  :ensure t)
+
+(use-package ein
+  :ensure t)
 
 (provide 'python-lang)
 
