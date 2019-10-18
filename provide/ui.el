6b0bb78f89fcf99af869597eaf1ab11ac80830d8
Avelino <t@avelino.xxx>
2019-10-18 10:50:20 -0300

    set UI to Emacs GUI

--- provide/ui.el
+++ provide/ui.el
@@ -4,20 +4,29 @@
 ;;
 ;;; Code:
 
+;; theme: dark
 ;; (use-package monokai-theme)
-;; (use-package dracula-theme)
-(use-package doom-themes
-  :defer t
-  :init (load-theme 'doom-molokai t))
-;; (load-theme 'ayu-theme t) ;; local [WIP] ref: https://github.com/dempfi/ayu
+(use-package dracula-theme)
+;; (use-package kaolin-themes
+;;   :ensure t
+;;   :defer t
+;;   :init (load-theme 'kaolin-bubblegum t))
+
+;; theme: light
 ;; (use-package spacemacs-theme
 ;;   :defer t
 ;;   :init (load-theme 'spacemacs-light t))
 
 (use-package color-identifiers-mode
+  :ensure t
   :diminish (color-identifiers-mode . "🎨 ")
   :init
   (add-hook 'after-init-hook 'global-color-identifiers-mode))
+
+(use-package rainbow-delimiters
+  :ensure t
+  :init
+  (add-hook 'prog-mode-hook #'rainbow-delimiters-mode))
 
 ;; smart-mode-line
 (use-package smart-mode-line
@@ -29,11 +38,7 @@
 
 ;; All the icons
 (use-package all-the-icons
-  :ensure t
-  :config
-  ;; The below command needs to be run only once manually to install the
-  ;; needed fonts (all-the-icons-install-fonts)
-  )
+  :ensure t)
 
 ;; Utility package that return vscode icons for Emacs
 (use-package vscode-icon
@@ -42,12 +47,39 @@
   :commands
   (vscode-icon-for-file))
 
-
 (use-package nlinum
+  :ensure t
   :hook (prog-mode . nlinum-mode)
   :config
   (setq nlinum-format "%4d "
 	nlinum-highlight-current-line 't))
+
+(use-package git-gutter-fringe+
+  :ensure t
+  :config
+  (global-git-gutter+-mode t))
+
+(use-package ergoemacs-status
+  :ensure t
+  :config
+  (ergoemacs-status-mode))
+
+(use-package spaceline-config           ; A beautiful mode line
+  :ensure spaceline
+  :config
+  (spaceline-emacs-theme))
+
+(use-package powerline                  ; The work-horse of Spaceline
+  :ensure t
+  :after spaceline-config
+  :config (setq
+           powerline-height (truncate (* 1.0 (frame-char-height)))
+           powerline-default-separator 'utf-8))
+
+(use-package fill-column-indicator
+  :ensure t
+  :config
+  (fci-mode))
 
 (provide 'ui)
 
