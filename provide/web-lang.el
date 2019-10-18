43c94647974ce591319e8adb30bb5b8f15787755
Avelino <t@avelino.xxx>
2019-10-18 10:46:15 -0300

    web: TS and RxJS mode

--- provide/web-lang.el
+++ provide/web-lang.el
@@ -5,6 +5,7 @@
 ;;; Code:
 
 (use-package web-mode
+  :ensure t
   :config
   (setq web-mode-markup-indent-offset 2
 	web-mode-css-indent-offset 2
@@ -13,7 +14,8 @@
 	web-mode-enable-engine-detection t
 	web-mode-enable-current-element-highlight t)
 
-  (add-to-list 'auto-mode-alist '("\\.js\\'" . web-mode))
+  ;; (add-to-list 'auto-mode-alist '("\\.js\\'" . web-mode))
+  ;; (add-to-list 'auto-mode-alist '("\\.jsx?$" . web-mode))
   (add-to-list 'auto-mode-alist '("\\.ts\\'" . web-mode))
   (add-to-list 'auto-mode-alist '("\\.html?\\'" . web-mode))
   (add-to-list 'auto-mode-alist '("\\.phtml\\'" . web-mode))
@@ -24,11 +26,21 @@
   (add-to-list 'auto-mode-alist '("\\.as[cp]x\\'" . web-mode))
   (add-to-list 'auto-mode-alist '("\\.erb\\'" . web-mode))
   (add-to-list 'auto-mode-alist '("\\.mustache\\'" . web-mode))
-  (add-to-list 'auto-mode-alist '("\\.djhtml\\'" . web-mode))
-  (add-to-list 'auto-mode-alist '("\\.jsx$" . web-mode))
+  (add-to-list 'auto-mode-alist '("\\.djhtml\\'" . web-mode)))
+
+(use-package rjsx-mode
+  :ensure t
+  :config
+  (add-to-list 'auto-mode-alist '(".*\.jsx?\'" . rjsx-mode))
+  (add-hook 'rjsx-mode-hook
+	    (lambda ()
+	      (setq indent-tabs-mode nil) ;;Use space instead of tab
+	      (setq js-indent-level 2) ;;space width is 2 (default is 4)
+	      (setq js2-strict-missing-semi-warning nil))) ;;disable the semicolon warning)
   (add-hook 'web-mode-hook 'jsx-flycheck))
 
-(use-package typescript-mode)
+(use-package typescript-mode
+  :ensure t)
 
 (defadvice web-mode-highlight-part (around tweak-jsx activate)
   (if (equal web-mode-content-type "jsx")
@@ -42,7 +54,11 @@
     (flycheck-select-checker 'jsxhint-checker)
     (flycheck-mode)))
 
-(use-package easy-hugo)
+(use-package easy-hugo
+  :ensure t
+  :config
+  (setq easy-hugo-markdown-extension "markdown"
+	easy-hugo-previewtime "300"))
 
 (provide 'web-lang)
 
